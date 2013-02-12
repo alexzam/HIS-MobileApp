882953a2345122c86213028ef6855c4ce7958b62
AlexZam <alexzzam@gmail.com>
2013-02-13 00:28:34 +0400

    Add settings action.

--- src/az/his/android/MyActivity.java
+++ src/az/his/android/MyActivity.java
@@ -3,6 +3,9 @@ package az.his.android;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import az.his.android.persist.DbHelper;
@@ -32,5 +35,17 @@ public class MyActivity extends Activity {
 
         Spinner spinner = (Spinner) findViewById(R.id.spnCategory);
         spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
