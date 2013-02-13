15d3ad1bbaa80537f44396585146401d72d4c0a3
AlexZam <alexzzam@gmail.com>
2013-02-13 14:46:17 +0400

    Add setting for URL and action for sync.

--- src/az/his/android/SettingsActivity.java
+++ src/az/his/android/SettingsActivity.java
@@ -1,12 +1,62 @@
 package az.his.android;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.*;
 
-public class SettingsActivity extends PreferenceActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private final List<String> summShowPrefs = Arrays.asList("str_url");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        for (String pref : summShowPrefs) {
+            initSummary(getPreferenceManager().findPreference(pref));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (summShowPrefs.contains(key)) {
+            updatePrefSummary(findPreference(key));
+        }
+    }
+
+    private void updatePrefSummary(Preference p) {
+        if (p instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) p;
+            p.setSummary(listPref.getEntry());
+        }
+        if (p instanceof EditTextPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) p;
+            p.setSummary(editTextPref.getText());
+        }
+    }
+
+    private void initSummary(Preference p) {
+        if (p instanceof PreferenceCategory) {
+            PreferenceCategory pCat = (PreferenceCategory) p;
+            for (int i = 0; i < pCat.getPreferenceCount(); i++) {
+                initSummary(pCat.getPreference(i));
+            }
+        } else {
+            updatePrefSummary(p);
+        }
     }
 }
